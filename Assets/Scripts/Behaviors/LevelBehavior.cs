50778da7f2dcabf50045a34e241fea8a85906bdd
Christophe Commeyne <christophecommeyne@nosuchcompany.ca>
2022-04-08 22:00:03 -0400

    Activate the Pole to switch level.

--- Assets/Scripts/Behaviors/LevelBehavior.cs
+++ Assets/Scripts/Behaviors/LevelBehavior.cs
@@ -6,8 +6,10 @@
 // Last change: 20/03/2022 @ 19:44
 // ==========================================================================
 
+using System.Linq;
 using NoSuchCompany.Games.SuperMario.Constants;
 using NoSuchCompany.Games.SuperMario.Diagnostics;
+using NoSuchCompany.Games.SuperMario.Services;
 using NoSuchCompany.Games.SuperMario.Services.Impl;
 using UnityEngine;
 
@@ -15,14 +17,35 @@ namespace NoSuchCompany.Games.SuperMario.Behaviors
 {
     public class LevelBehavior : MonoBehaviour
     {
-        public void OnTriggerEnter2D(Collider2D otherCollider2D)
+        //  Private fields.
+        private readonly IRaycaster _raycaster;
+
+        //  Unity injected properties.
+        public LayerMask collisionMask;
+
+        public LevelBehavior()
         {
-            AppLogger.Write(LogsLevels.None, $"Touched! {otherCollider2D.tag}");
+            _raycaster = new Raycaster();
+        }
+        
+        public void Start()
+        {
+            var boxCollider2D = GetComponent<BoxCollider2D>();
             
-            if (otherCollider2D.CompareTag(Tags.Player))
+            _raycaster.Initialize(boxCollider2D, 3);
+        }
+
+        public void Update()
+        {
+            Vector3 noMovement = Vector3.zero;
+            _raycaster.ApplyCollisions(ref noMovement, collisionMask);
+
+            AppLogger.Write(LogsLevels.None, $"Collisions: {_raycaster.Collisions.LeftCollisions.Count()}");
+            
+            if (_raycaster.Collisions.LeftCollisions.Contains(Tags.Player))
             {
+                AppLogger.Write(LogsLevels.None, $"HIT!!!");
                 LevelManager.Instance.LoadNextLevel();
-                return;
             }
         }
     }
